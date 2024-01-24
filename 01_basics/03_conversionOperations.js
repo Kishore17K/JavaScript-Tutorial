d31db8714d226a9af77ddd5e835cfacc43030b90
Kishore17K <155523672+Kishore17K@users.noreply.github.com>
2024-01-24 15:39:44 +0000

    c

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,34 @@
+let score ="kishor"
+
+// console.log(typeof score)
+//  console.log(typeof(score))
+
+//  let valueInNumber = Number (score)
+//  console.log(typeof valueInNumber)
+
+//   console.log(valueInNumber)
+
+
+  // "33" => 33
+  //"33abc" => NaN(Not a Number)
+  //true => 1 ; false =>0
+
+  let isLoggedIn =" kishor"
+
+  let booleanIsLoggedIn  = Boolean(isLoggedIn)
+  console.log(booleanIsLoggedIn);
+
+
+  // 1 => when we set the value is "1" then output show the  "true" value and we set "0" then output is "false"
+  // "" => when we set the empty string then output show the "false value"
+  // "kishor" => when we set the string value rthen output show the string value 
+
+
+
+  let sumNumber = 33
+
+   let stringNumber = String (sumNumber) // convert number into string 
+   console.log(stringNumber);
+   console.log(typeof stringNumber)
+
+
